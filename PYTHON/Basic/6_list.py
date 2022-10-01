66b1f25c91338c91b0d15bb4fab54f1dad6a0b27
ruhulashazid <ruhulaminshazid@gmail.com>
2022-10-01 12:08:36 +0600

    practice 1 day3

---
+++ PYTHON/Basic/6_list.py
@@ -0,0 +1,8 @@
+li = [1,2,3,4,5,6,7,8,9,10]
+li = ['a','b','c','d','e','f','g','h','i','j']
+li3 = [1,2,3.2,'a','b','c',True,False]
+
+#more examples
+amazon_cart = ['notebooks','sunglasses','toys','grapes']
+print(amazon_cart[0])
+
